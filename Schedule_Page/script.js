4d8cfc3f29c539d7d0c0054bed20c41f9710e95d
shivchopra37 <shivchopra635@gmail.com>
2024-07-30 12:00:58 +0530

    modified page

--- Schedule_Page/script.js
+++ Schedule_Page/script.js
@@ -7,4 +7,8 @@ function navigate(direction) {
     
     document.getElementById('prev').disabled = currentDay === 1;
     document.getElementById('next').disabled = currentDay === 6;
+
+    for (let i = 1; i <= 6; i++) {
+        document.getElementById(`day${i}`).style.display = (i === currentDay) ? 'flex' : 'none';
+    }
 }
