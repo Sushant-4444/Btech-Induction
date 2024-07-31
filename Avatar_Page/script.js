debf0110c770d828b975e49e1fff29dd22f2c7ae
Sushant-4444 <arorasushant4444@gmail.com>
2024-08-01 03:53:52 +0530

    updated

--- Avatar_Page/script.js
+++ Avatar_Page/script.js
@@ -3,6 +3,14 @@ const face_options=document.querySelectorAll('.avatar-option-face');
 face_options.forEach(option=>{
     option.addEventListener('click',(e)=>{
         document.querySelector('.Face').setAttribute('src',e.target.getAttribute('src'));
+        e.target.style.border='2px solid lightgreen';
+        // remove border from other options
+        face_options.forEach(option=>{
+            if(option!=e.target){
+                option.style.border='none';
+            }
+        }
+        );
     });
 });
 
@@ -11,6 +19,13 @@ const eye_options=document.querySelectorAll('.avatar-option-eye');
 eye_options.forEach(option=>{
     option.addEventListener('click',(e)=>{
         document.querySelector('.Eyes').setAttribute('src',e.target.getAttribute('src'));
+        e.target.style.border='2px solid lightgreen';
+        // remove border from other options
+        eye_options.forEach(option=>{
+            if(option!=e.target){
+                option.style.border='none';
+            }
+        });
     });
 });
 
@@ -19,15 +34,52 @@ const body_options=document.querySelectorAll('.avatar-option-body');
 body_options.forEach(option=>{
     option.addEventListener('click',(e)=>{
         document.querySelector('.Body').setAttribute('src',e.target.getAttribute('src'));
+        e.target.style.border='2px solid lightgreen';
+        // remove border from other options
+        body_options.forEach(option=>{
+            if(option!=e.target){
+                option.style.border='none';
+            }
+        });
     });
 });
+
+const beard_options=document.querySelectorAll('.avatar-option-beard');
+
+beard_options.forEach(option=>{
+    option.addEventListener('click',(e)=>{
+        document.querySelector('.Beard').setAttribute('src',e.target.getAttribute('src'));
+        e.target.style.border='2px solid lightgreen';
+        // remove border from other options
+        beard_options.forEach(option=>{
+            if(option!=e.target){
+                option.style.border='none';
+            }
+        });
+    });
+});
+const spec_options=document.querySelectorAll('.avatar-option-specs');
+
+spec_options.forEach(option=>{
+    option.addEventListener('click',(e)=>{
+        document.querySelector('.Accessory').setAttribute('src',e.target.getAttribute('src'));
+        e.target.style.border='2px solid lightgreen';
+        // remove border from other options
+        spec_options.forEach(option=>{
+            if(option!=e.target){
+                option.style.border='none';
+            }
+        });
+    });
+});
+
 
 const option_buttons=document.querySelectorAll('.options-bar');
 
 option_buttons.forEach(button=>{
     button.addEventListener('click',(e)=>{
         // console.log(e.target.getAttribute('id'));
-        const btnid=e.target.getAttribute('id').slice(0,4);
+        const btnid=e.target.getAttribute('id').slice(0,e.target.getAttribute('id').indexOf('B'));
         console.log(btnid);
         // target=id.slice(0,4);
         const target_div=document.querySelector(`#${btnid}`);
