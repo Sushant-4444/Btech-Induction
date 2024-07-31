c08e5f1733780f8279e66a0a5c0a78371f843ead
shivchopra37 <shivchopra635@gmail.com>
2024-07-31 13:21:16 +0530

    change in schedule page

--- Schedule_Page/script.js
+++ Schedule_Page/script.js
@@ -1,10 +1,11 @@
 let currentDay = 1;
+const dates = ["05/08/24", "06/08/24", "07/08/24", "08/08/24", "09/08/24", "10/08/24"];
 
 function navigate(direction) {
     currentDay += direction;
 
-    document.getElementById('day').innerText = `Day ${currentDay}`;
-    
+    document.getElementById('day').innerText = `Day ${currentDay} (${dates[currentDay - 1]})`;
+
     document.getElementById('prev').disabled = currentDay === 1;
     document.getElementById('next').disabled = currentDay === 6;
 
